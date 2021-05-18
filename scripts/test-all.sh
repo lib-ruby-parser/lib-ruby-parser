5df55332684b3e33d5294c9dcaf5c7e62f78e4fa
Ilya Bylich <ibylich@gmail.com>
2021-05-18 21:24:25 +0300

    run all tests on nightly/external-structures

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -12,17 +12,16 @@ rustup default nightly
 export ASAN_OPTIONS=detect_leaks=1
 export RUSTFLAGS=-Zsanitizer=address
 
-export LIB_RUBY_PARSER_PTR_SIZE=changeme
-export LIB_RUBY_PARSER_MAYBE_PTR_SIZE=changeme
-export LIB_RUBY_PARSER_LIST_SIZE=changeme
-export LIB_RUBY_PARSER_STRING_PTR_SIZE=changeme
-cargo test --features "compile-with-external-structures,link-external-c-structures,nightly-features" --test parser_test
-cargo test --features "compile-with-external-structures,link-external-c-structures,nightly-features" -- containers
+LIB_RUBY_PARSER_PTR_SIZE=8 \
+LIB_RUBY_PARSER_MAYBE_PTR_SIZE=8 \
+LIB_RUBY_PARSER_LIST_SIZE=24 \
+LIB_RUBY_PARSER_STRING_PTR_SIZE=16 \
+LIB_RUBY_PARSER_SHARED_BYTE_LIST_SIZE=16 \
+    cargo test --features "compile-with-external-structures,link-external-c-structures,nightly-features"
 
-export LIB_RUBY_PARSER_PTR_SIZE=8
-export LIB_RUBY_PARSER_MAYBE_PTR_SIZE=8
-export LIB_RUBY_PARSER_LIST_SIZE=24
-export LIB_RUBY_PARSER_STRING_PTR_SIZE=8
-export LIB_RUBY_PARSER_SHARED_BYTE_LIST_SIZE=16
-cargo test --features "compile-with-external-structures,link-external-cpp-structures,nightly-features" --test parser_test
-cargo test --features "compile-with-external-structures,link-external-cpp-structures,nightly-features" --lib -- containers
+LIB_RUBY_PARSER_PTR_SIZE=8 \
+LIB_RUBY_PARSER_MAYBE_PTR_SIZE=8 \
+LIB_RUBY_PARSER_LIST_SIZE=24 \
+LIB_RUBY_PARSER_STRING_PTR_SIZE=8 \
+LIB_RUBY_PARSER_SHARED_BYTE_LIST_SIZE=16 \
+    cargo test --features "compile-with-external-structures,link-external-cpp-structures,nightly-features"
