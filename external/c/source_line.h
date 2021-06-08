8d670f25730647e90fe0cdb0745d55d5cac40ec1
Ilya Bylich <ibylich@gmail.com>
2021-06-08 20:51:00 +0300

    implemented SourceLine in c/cpp

--- external/c/source_line.h
+++ external/c/source_line.h
@@ -4,12 +4,31 @@
 #include "declare_dummy_struct.h"
 #include "declare_blob.h"
 #include "declare_list.h"
+#include <stdbool.h>
 
-DECLARE_DUMMY_STRUCT(SourceLine, 24);
+typedef struct
+{
+    uint64_t start;
+    uint64_t end;
+    bool ends_with_eof;
+} SourceLine;
+_Static_assert(sizeof(SourceLine) == 24, "sizeof(SourceLine) == 24");
 DECLARE_BLOB_FOR(SourceLine);
 
 DECLARE_LIST_OF(SourceLine_BLOB, LIST_OF_SourceLine);
 DECLARE_BLOB_FOR(LIST_OF_SourceLine);
 _Static_assert(sizeof(LIST_OF_SourceLine) == 24, "sizeof(LIST_OF_SourceLine) == 24");
+
+// API
+
+SourceLine_BLOB lib_ruby_parser_source_line_new(uint64_t start, uint64_t end, bool ends_with_eof);
+
+uint64_t lib_ruby_parser_source_line_get_start(SourceLine_BLOB blob);
+uint64_t lib_ruby_parser_source_line_get_end(SourceLine_BLOB blob);
+bool lib_ruby_parser_source_line_get_ends_with_eof(SourceLine_BLOB blob);
+
+SourceLine_BLOB lib_ruby_parser_source_line_set_start(SourceLine_BLOB blob, uint64_t start);
+SourceLine_BLOB lib_ruby_parser_source_line_set_end(SourceLine_BLOB blob, uint64_t end);
+SourceLine_BLOB lib_ruby_parser_source_line_set_ends_with_eof(SourceLine_BLOB blob, bool ends_with_eof);
 
 #endif // LIB_RUBY_PARSER_EXTERNAL_C_SOURCE_LINE_H
