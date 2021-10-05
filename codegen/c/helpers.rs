4bb6edb32b20b9bcdffa9af4c41cbd4f11cd2e76
Ilya Bylich <ibylich@gmail.com>
2021-10-06 01:51:48 +0300

    use template for nodes.h

--- codegen/c/helpers.rs
+++ codegen/c/helpers.rs
@@ -20,22 +20,6 @@ pub(crate) mod nodes {
         pub(crate) fn field_name(field: &lib_ruby_parser_nodes::NodeField) -> String {
             default_node_helpers::fields::field_name(field)
         }
-
-        pub(crate) fn field_type(field: &lib_ruby_parser_nodes::NodeField) -> &str {
-            use lib_ruby_parser_nodes::NodeFieldType;
-
-            match field.field_type {
-                NodeFieldType::Node => "LIB_RUBY_PARSER_NodePtr",
-                NodeFieldType::Nodes => "LIB_RUBY_PARSER_NodeList",
-                NodeFieldType::MaybeNode { .. } => "LIB_RUBY_PARSER_MaybeNodePtr",
-                NodeFieldType::Loc => "LIB_RUBY_PARSER_Loc",
-                NodeFieldType::MaybeLoc => "LIB_RUBY_PARSER_MaybeLoc",
-                NodeFieldType::Str { .. } => "LIB_RUBY_PARSER_StringPtr",
-                NodeFieldType::MaybeStr { .. } => "LIB_RUBY_PARSER_MaybeStringPtr",
-                NodeFieldType::StringValue => "LIB_RUBY_PARSER_Bytes",
-                NodeFieldType::U8 => "LIB_RUBY_PARSER_Byte",
-            }
-        }
     }
 
     pub(crate) fn enum_variant_name(node: &lib_ruby_parser_nodes::Node) -> String {
