c4052422eb14dc246e2cd28d317689ec32dd42b6
Ilya Bylich <ibylich@gmail.com>
2021-10-05 18:32:27 +0300

    use templates for binding_nodes.c

--- codegen/c/helpers.rs
+++ codegen/c/helpers.rs
@@ -36,58 +36,6 @@ pub(crate) mod nodes {
                 NodeFieldType::U8 => "LIB_RUBY_PARSER_Byte",
             }
         }
-
-        pub(crate) fn internal_field_type(field: &lib_ruby_parser_nodes::NodeField) -> &str {
-            use lib_ruby_parser_nodes::NodeFieldType;
-
-            match field.field_type {
-                NodeFieldType::Node => "LIB_RUBY_PARSER_Ptr",
-                NodeFieldType::Nodes => "LIB_RUBY_PARSER_NodeList",
-                NodeFieldType::MaybeNode { .. } => "LIB_RUBY_PARSER_MaybePtr",
-                NodeFieldType::Loc => "LIB_RUBY_PARSER_Loc",
-                NodeFieldType::MaybeLoc => "LIB_RUBY_PARSER_MaybeLoc",
-                NodeFieldType::Str { .. } => "LIB_RUBY_PARSER_StringPtr",
-                NodeFieldType::MaybeStr { .. } => "LIB_RUBY_PARSER_MaybeStringPtr",
-                NodeFieldType::StringValue => "LIB_RUBY_PARSER_Bytes",
-                NodeFieldType::U8 => "LIB_RUBY_PARSER_Byte",
-            }
-        }
-
-        pub(crate) fn pack_field_fn(field: &lib_ruby_parser_nodes::NodeField) -> &str {
-            use lib_ruby_parser_nodes::NodeFieldType;
-
-            match field.field_type {
-                NodeFieldType::Node => "PACK_Ptr",
-                NodeFieldType::Nodes => "PACK_NodeList",
-                NodeFieldType::MaybeNode { .. } => "PACK_MaybePtr",
-                NodeFieldType::Loc => "PACK_Loc",
-                NodeFieldType::MaybeLoc => "PACK_MaybeLoc",
-                NodeFieldType::Str { .. } => "PACK_StringPtr",
-                NodeFieldType::MaybeStr { .. } => "PACK_MaybeStringPtr",
-                NodeFieldType::StringValue => "PACK_Bytes",
-                NodeFieldType::U8 => "PACK_Byte",
-            }
-        }
-
-        pub(crate) fn unpack_field_fn(field: &lib_ruby_parser_nodes::NodeField) -> &str {
-            use lib_ruby_parser_nodes::NodeFieldType;
-
-            match field.field_type {
-                NodeFieldType::Node => "UNPACK_Ptr",
-                NodeFieldType::Nodes => "UNPACK_NodeList",
-                NodeFieldType::MaybeNode { .. } => "UNPACK_MaybePtr",
-                NodeFieldType::Loc => "UNPACK_Loc",
-                NodeFieldType::MaybeLoc => "UNPACK_MaybeLoc",
-                NodeFieldType::Str { .. } => "UNPACK_StringPtr",
-                NodeFieldType::MaybeStr { .. } => "UNPACK_MaybeStringPtr",
-                NodeFieldType::StringValue => "UNPACK_Bytes",
-                NodeFieldType::U8 => "UNPACK_Byte",
-            }
-        }
-
-        pub(crate) fn blob_type(field: &lib_ruby_parser_nodes::NodeField) -> String {
-            format!("{}_BLOB", internal_field_type(field))
-        }
     }
 
     pub(crate) fn enum_variant_name(node: &lib_ruby_parser_nodes::Node) -> String {
--- codegen/fns.rs
+++ codegen/fns.rs
@@ -16,6 +16,19 @@ mod nodes {
     pub(crate) fn lower_name(node: &Node) -> String {
         node.lower_name()
     }
+
+    pub(crate) fn c_enum_variant_name(node: &Node) -> String {
+        format!("LIB_RUBY_PARSER_NODE_{}", node.upper_name())
+    }
+
+    pub(crate) fn c_union_member_name(node: &Node) -> String {
+        let lower = node.lower_name();
+        match &lower[..] {
+            "and" | "break" | "case" | "class" | "const" | "false" | "float" | "for" | "if"
+            | "int" | "or" | "return" | "true" | "while" => format!("{}_", lower),
+            other => other.to_owned(),
+        }
+    }
 }
 
 mod node_fields {
@@ -23,6 +36,79 @@ mod node_fields {
 
     pub(crate) fn name(node_with_field: &NodeWithField) -> String {
         node_with_field.field.snakecase_name.to_string()
+    }
+
+    #[cfg(feature = "lib-ruby-parser-bindings")]
+    pub(crate) fn c_name(node_with_field: &NodeWithField) -> String {
+        lib_ruby_parser_bindings::helpers::nodes::fields::field_name(&node_with_field.field)
+    }
+
+    pub(crate) fn c_pack_fn_name(node_with_field: &NodeWithField) -> String {
+        use lib_ruby_parser_nodes::NodeFieldType::*;
+
+        match node_with_field.field.field_type {
+            Node => "PACK_Ptr",
+            Nodes => "PACK_NodeList",
+            MaybeNode { .. } => "PACK_MaybePtr",
+            Loc => "PACK_Loc",
+            MaybeLoc => "PACK_MaybeLoc",
+            Str { .. } => "PACK_StringPtr",
+            MaybeStr { .. } => "PACK_MaybeStringPtr",
+            StringValue => "PACK_Bytes",
+            U8 => "PACK_Byte",
+        }
+        .to_string()
+    }
+
+    pub(crate) fn c_unpack_fn_name(node_with_field: &NodeWithField) -> String {
+        use lib_ruby_parser_nodes::NodeFieldType::*;
+
+        match node_with_field.field.field_type {
+            Node => "UNPACK_Ptr",
+            Nodes => "UNPACK_NodeList",
+            MaybeNode { .. } => "UNPACK_MaybePtr",
+            Loc => "UNPACK_Loc",
+            MaybeLoc => "UNPACK_MaybeLoc",
+            Str { .. } => "UNPACK_StringPtr",
+            MaybeStr { .. } => "UNPACK_MaybeStringPtr",
+            StringValue => "UNPACK_Bytes",
+            U8 => "UNPACK_Byte",
+        }
+        .to_string()
+    }
+
+    pub(crate) fn c_field_type(node_with_field: &NodeWithField) -> String {
+        use lib_ruby_parser_nodes::NodeFieldType::*;
+
+        match node_with_field.field.field_type {
+            Node => "LIB_RUBY_PARSER_NodePtr",
+            Nodes => "LIB_RUBY_PARSER_NodeList",
+            MaybeNode { .. } => "LIB_RUBY_PARSER_MaybeNodePtr",
+            Loc => "LIB_RUBY_PARSER_Loc",
+            MaybeLoc => "LIB_RUBY_PARSER_MaybeLoc",
+            Str { .. } => "LIB_RUBY_PARSER_StringPtr",
+            MaybeStr { .. } => "LIB_RUBY_PARSER_MaybeStringPtr",
+            StringValue => "LIB_RUBY_PARSER_Bytes",
+            U8 => "LIB_RUBY_PARSER_Byte",
+        }
+        .to_string()
+    }
+
+    pub(crate) fn c_blob_type(node_with_field: &NodeWithField) -> String {
+        use lib_ruby_parser_nodes::NodeFieldType::*;
+
+        match node_with_field.field.field_type {
+            Node => "LIB_RUBY_PARSER_Ptr_BLOB",
+            Nodes => "LIB_RUBY_PARSER_NodeList_BLOB",
+            MaybeNode { .. } => "LIB_RUBY_PARSER_MaybePtr_BLOB",
+            Loc => "LIB_RUBY_PARSER_Loc_BLOB",
+            MaybeLoc => "LIB_RUBY_PARSER_MaybeLoc_BLOB",
+            Str { .. } => "LIB_RUBY_PARSER_StringPtr_BLOB",
+            MaybeStr { .. } => "LIB_RUBY_PARSER_MaybeStringPtr_BLOB",
+            StringValue => "LIB_RUBY_PARSER_Bytes_BLOB",
+            U8 => "LIB_RUBY_PARSER_Byte_BLOB",
+        }
+        .to_string()
     }
 }
 
@@ -45,16 +131,21 @@ mod messages {
 mod message_fields {
     use super::*;
 
-    #[cfg(feature = "lib-ruby-parser-bindings")]
     pub(crate) fn name(message_with_field: &MessageWithField) -> String {
+        message_with_field.field.snakecase_name.to_owned()
+    }
+
+    #[cfg(feature = "lib-ruby-parser-bindings")]
+    pub(crate) fn c_name(message_with_field: &MessageWithField) -> String {
         lib_ruby_parser_bindings::helpers::messages::fields::field_name(&message_with_field.field)
             .to_string()
     }
 
     pub(crate) fn c_field_type(message_with_field: &MessageWithField) -> String {
+        use lib_ruby_parser_nodes::MessageFieldType::*;
         match message_with_field.field.field_type {
-            lib_ruby_parser_nodes::MessageFieldType::Str => "LIB_RUBY_PARSER_StringPtr",
-            lib_ruby_parser_nodes::MessageFieldType::Byte => "LIB_RUBY_PARSER_Byte",
+            Str => "LIB_RUBY_PARSER_StringPtr",
+            Byte => "LIB_RUBY_PARSER_Byte",
         }
         .to_string()
     }
@@ -70,15 +161,24 @@ pub(crate) fn build() -> TemplateFns {
     fns.register_helper("node-camelcase-name", nodes::camelcase_name);
     fns.register_helper("node-upper-name", nodes::upper_name);
     fns.register_helper("node-lower-name", nodes::lower_name);
+    fns.register_helper("node-c-enum-variant-name", nodes::c_enum_variant_name);
+    fns.register_helper("node-c-union-member-name", nodes::c_union_member_name);
 
     fns.register_helper("node-field-name", node_fields::name);
+    #[cfg(feature = "lib-ruby-parser-bindings")]
+    fns.register_helper("node-field-c-name", node_fields::c_name);
+    fns.register_helper("node-field-c-field-type", node_fields::c_field_type);
+    fns.register_helper("node-field-c-blob-type", node_fields::c_blob_type);
+    fns.register_helper("node-field-c-pack-fn-name", node_fields::c_pack_fn_name);
+    fns.register_helper("node-field-c-unpack-fn-name", node_fields::c_unpack_fn_name);
 
     fns.register_helper("message-camelcase-name", messages::camelcase_name);
     fns.register_helper("message-upper-name", messages::upper_name);
     fns.register_helper("message-lower-name", messages::lower_name);
 
-    #[cfg(feature = "lib-ruby-parser-bindings")]
     fns.register_helper("message-field-name", message_fields::name);
+    #[cfg(feature = "lib-ruby-parser-bindings")]
+    fns.register_helper("message-field-c-name", message_fields::c_name);
     fns.register_helper("message-field-c-field-type", message_fields::c_field_type);
     fns.register_helper("message-field-c-blob-type", message_fields::c_blob_type);
 
