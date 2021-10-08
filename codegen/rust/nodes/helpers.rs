2e4afcbdcfd2c388a971f01fc9d24617c62e7ede
Ilya Bylich <ibylich@gmail.com>
2021-10-08 20:40:53 +0300

    use template for src/traverse/visitor/visit_gen.rs

--- codegen/rust/nodes/helpers.rs
+++ codegen/rust/nodes/helpers.rs
@@ -7,21 +7,3 @@ pub(crate) fn filename(node: &lib_ruby_parser_nodes::Node) -> String {
         _ => lower,
     }
 }
-
-pub(crate) fn struct_name(node: &lib_ruby_parser_nodes::Node) -> String {
-    let camelcase_name = node.camelcase_name.to_owned();
-
-    match &camelcase_name[..] {
-        "Self" => format!("{}_", camelcase_name),
-        _ => camelcase_name,
-    }
-}
-
-pub(crate) fn node_field_name(field: &lib_ruby_parser_nodes::NodeField) -> String {
-    let name = field.snakecase_name.to_owned();
-
-    match &name[..] {
-        "const" | "as" | "else" => format!("{}_", name),
-        _ => name,
-    }
-}
