e433d58cfefa62cf87a70b356b33ff87beabcf92
Ilya Bylich <ibylich@gmail.com>
2021-10-28 22:50:50 +0300

    do not include decoded input in ParserResult debug impl

--- src/parser_result.rs
+++ src/parser_result.rs
@@ -6,7 +6,6 @@ use crate::Node;
 use crate::Token;
 
 /// Combination of all data that `Parser` can give you
-#[derive(Debug)]
 #[repr(C)]
 pub struct ParserResult {
     /// Abstract Syntax Tree that was constructed from you code.
@@ -37,4 +36,16 @@ pub struct ParserResult {
     /// Pass **this** data to `Loc::source`, otherwise you'll get
     /// incorrect source ranges.
     pub input: DecodedInput,
+}
+
+impl std::fmt::Debug for ParserResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ParserResult")
+            .field("ast", &self.ast)
+            .field("tokens", &self.tokens)
+            .field("diagnostics", &self.diagnostics)
+            .field("comments", &self.comments)
+            .field("magic_comments", &self.magic_comments)
+            .finish()
+    }
 }
